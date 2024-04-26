2debf5fedee8a44bb43ddb002e1512522444274f
DiegoGReyna <71367954+DiegoGReyna@users.noreply.github.com>
2024-04-26 05:12:26 -0600

    primer push
    primera vercion de mi portafolio personal

---
+++ scripts/script.js
@@ -0,0 +1,11 @@
+
+// gsap.to(".skill",{
+//     rotation:360,
+//     duration: 2,
+// })
+
+function cleanInputs(){
+  // document.getElementById("IdEmail").value=""
+  // document.getElementById("IdMessage").value=""
+  // document.getElementById("IdName").value=""
+}
